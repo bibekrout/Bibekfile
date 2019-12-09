f6a11e992e0de64c55c54fbe3e5303a6b9b09628
testing <testing@gmail.com>
2019-12-09 14:59:46 +0530

    impfile

---
+++ src/gitpackage/gitclass.java
@@ -0,0 +1,15 @@
+package gitpackage;
+
+public class gitclass
+{
+
+	public static void main(String[] args) 
+	{
+		String str="bibek";
+		String st="rout";
+		System.out.println(str+" "+st);
+
+
+	}
+
+}
